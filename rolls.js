1e493a49bc39337f53533684d2dc7e5b2ec85c2b
Jim G <31528489+daftsort@users.noreply.github.com>
2019-05-27 18:28:43 -0500

    Add files via upload

---
+++ rolls.js
@@ -0,0 +1,6 @@
+var d_four_roll = Math.floor(Math.random() * 4) + 1;
+var d_six_roll = Math.floor(Math.random() * 6) + 1;
+var d_eight_roll = Math.floor(Math.random() * 8) + 1;
+var d_ten_roll = Math.floor(Math.random() * 10) + 1;
+var d_twenty_roll = Math.floor(Math.random() * 20) + 1;
+var d_hundred_roll = Math.floor(Math.random() * 100) + 1;
