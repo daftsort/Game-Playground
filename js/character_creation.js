3dfc4e35bca090f4139e7422c9eeae6aeaeaee92
Jim G <31528489+daftsort@users.noreply.github.com>
2019-05-27 19:12:14 -0500

    Update character_creation.js

--- js/character_creation.js
+++ js/character_creation.js
@@ -1,5 +1,22 @@
 src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
 $.getScript('character_template.js'), function () {
+    function setArchetypes(cunning, confidence, combat, concealment){
+        this.cunning = cunning;
+        this.confidence = confidence;
+        this.combat = combat;
+        this.concealment = concealment;
+        return cunning, confidence, combat, concealment;
+    }
+    function setResources(health, stamina, mana){
+        this.health = health;
+        this.stamina = stamina;
+        this.mana = mana;
+    }
+    function setAttributes(strength, dexterity, intelligence){
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence; 
+    }
     var PlayerOne = new player_character;
     PlayerOne.name = prompt("Please enter your name: ");
     PlayerOne.race = prompt("What is your race? (human, elf, dwarf, lupe)")
@@ -22,112 +39,67 @@ $.getScript('character_template.js'), function () {
     PlayerOne.archetype = prompt("What is your class? (cunning, confidence, combat, concealment)")
     switch (PlayerOne.race) {
         case elf:
-            PlayerOne.health = 8;
-            PlayerOne.stamina = 8;
-            PlayerOne.mana = 14;
+            PlayerOne.setResources(8, 8, 14);
             break;
         case dwarf:
-            PlayerOne.health = 10;
-            PlayerOne.stamina = 14;
-            PlayerOne.mana = 6;
+            PlayerOne.setResources(10, 14, 6);
             break;
         case lupe:
-            PlayerOne.health = 14;
-            PlayerOne.stamina = 10;
-            PlayerOne.mana = 6;
+            PlayerOne.setResources(14, 10, 6);
             break;
         case human:
-            PlayerOne.health = 10;
-            PlayerOne.stamina = 12;
-            PlayerOne.mana = 8;
+            PlayerOne.setResources(10, 12, 8);
             break;
         default:
-            PlayerOne.health = 8;
-            PlayerOne.stamina = 8;
-            PlayerOne.mana = 8;
+            PlayerOne.setResources(8, 8, 8);
     }
 
     switch (PlayerOne.subrace) {
         case night:
-            PlayerOne.strength = 10;
-            PlayerOne.dexterity = 12;
-            PlayerOne.intelligence = 12;
+            PlayerOne.setAttributes(10, 12, 12);
             break;
         case sea:
-            PlayerOne.strength = 8;
-            PlayerOne.dexterity = 14
-            PlayerOne.intelligence = 12;
+            PlayerOne.setAttributes(8, 14, 12);
             break;
         case high:
-            PlayerOne.strength = 8;
-            PlayerOne.dexterity = 10;
-            PlayerOne.intelligence = 16;
+            PlayerOne.setAttributes(8, 10, 16);
             break;
         case wood:
-            PlayerOne.strength = 8;
-            PlayerOne.dexterity = 16;
-            PlayerOne.intelligence = 10;
+            PlayerOne.setAttributes(8, 16, 10);
             break;
         case hill:
-            PlayerOne.strength = 14;
-            PlayerOne.dexterity = 10;
-            PlayerOne.intelligence = 10;
+            PlayerOne.setAttributes(14, 10, 10);
             break;
         case mountain:
-            PlayerOne.strength = 16;
-            PlayerOne.dexterity = 10;
-            PlayerOne.intelligence = 8;
+            PlayerOne.setAttributes(16, 10, 8);
             break;
         case voyager:
-            PlayerOne.strength = 12;
-            PlayerOne.dexterity = 10;
-            PlayerOne.intelligence = 12;
+            PlayerOne.setAttributes(12, 10, 12);
             break;
         case trader:
-            PlayerOne.strength = 10;
-            PlayerOne.dexterity = 10;
-            PlayerOne.intelligence = 14;
+            PlayerOne.setAttributes(10, 10, 14);
             break;
         case warrior:
-            PlayerOne.strength = 14;
-            PlayerOne.dexterity = 14;
-            PlayerOne.intelligence = 6;
+            PlayerOne.setAttributes(14, 14, 6);
             break;
         default:
-            PlayerOne.strength = 10;
-            PlayerOne.dexterity = 10;
-            PlayerOne.intelligence = 10;    
+            PlayerOne.setAttributes(10, 10, 10);
     }
 
-    switch (PlayerOne.archetype){
+    switch (PlayerOne.archetype) {
         case cunning:
-            PlayerOne.cunning_level = 4;
-            PlayerOne.confidence_level = 1;
-            PlayerOne.combat_level = 2;
-            PlayerOne.concealment_level = 3;
+            PlayerOne.setArchetypes(4, 1, 2, 3);
             break;
-        case confidence: 
-            PlayerOne.cunning_level = 1;
-            PlayerOne.confidence_level = 4;
-            PlayerOne.combat_level = 3;
-            PlayerOne.concealment_level = 2;
+        case confidence:
+            PlayerOne.setArchetypes(1, 4, 3, 2);
             break;
         case combat:
-            PlayerOne.cunning_level = 2;
-            PlayerOne.confidence = 3;
-            PlayerOne.combat_level = 4;
-            PlayerOne.concealment_level = 1;
+            PlayerOne.setArchetypes(2, 3, 4, 1);
             break;
         case concealment:
-            PlayerOne.cunning_level = 3;
-            PlayerOne.confidence_level = 2;
-            PlayerOne.combat_level = 1
-            PlayerOne.concealment_level = 4;
+            PlayerOne.setArchetypes(3, 2, 1, 4);
             break;
         default:
-            PlayerOne.cunning_level = 1;
-            PlayerOne.confidence_level = 1;
-            PlayerOne.combat_level = 1;
-            PlayerOne.concealment_level = 1;
+            PlayerOne.setArchetypes(1, 1, 1, 1);
     }
 }
