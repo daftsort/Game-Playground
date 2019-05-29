99e9350945ca811076c1e362e939f64b7a93ea96
Jim G <31528489+daftsort@users.noreply.github.com>
2019-05-29 10:28:37 -0500

    Update character_config.js

--- js/character_config.js
+++ js/character_config.js
@@ -37,9 +37,17 @@ playerSubRaces = {
     night: setAttributes(10,12,12),
     sea: setAttributes(8,14,12),
     high: setAttributes(8,10,16),
-    wood: setAttributes(8,16,10)
+    wood: setAttributes(8,16,10),
+    hill: setAttributes(14,10,10),
+    mountain: setAttributes(16,10,8),
+    voyager: setAttributes(12,10,12),
+    trader: setAttributes(10,10,14),
+    warrior: setAttributes(14,14,6)
 };
 
 playerArchetypes = {
-
+    cunning: setArchetypes(4,1,2,3),
+    confidence: setArchetypes(1,4,3,2),
+    combat: setArchetypes(2,3,4,1),
+    concealment: setArchetypes(3,2,1,4)
 };
