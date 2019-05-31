7a63a2d69624e46c3a41d3024142890b9ecc9582
Jim G <31528489+daftsort@users.noreply.github.com>
2019-05-30 21:25:03 -0500

    Delete rolls.js

--- js/rolls.js
+++
@@ -1,6 +0,0 @@
-var d_four_roll = Math.floor(Math.random() * 4) + 1;
-var d_six_roll = Math.floor(Math.random() * 6) + 1;
-var d_eight_roll = Math.floor(Math.random() * 8) + 1;
-var d_ten_roll = Math.floor(Math.random() * 10) + 1;
-var d_twenty_roll = Math.floor(Math.random() * 20) + 1;
-var d_hundred_roll = Math.floor(Math.random() * 100) + 1;
